458ebc9d097071d8e4a7db7fa33e09b26d235a4d
Dominic Brown <yeahdom@gmail.com>
2017-05-05 10:30:15 -0700

    add code to save volunteers to the database

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -2,22 +2,24 @@ class Volunteer
   attr_accessor(:name, :id)
 
   def initialize(init_hash)
-    @name = init_hash['name']
-    @id = init_hash['id']
+    @name = init_hash[:name]
+    @id = init_hash[:id]
   end
 
-  def self.all 
+  def self.all
     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
     volunteer_hashes_arr = []
-    returned_volunteers.each() do |pgtuple|
-      name = list["name"]
-      id = list["id"].to_i()
+    returned_volunteers.each() do |tuple|
+      name = tuple["name"]
+      id = tuple["id"].to_i()
       volunteer_hashes_arr.push(Volunteer.new({:name => name, :id => id}))
     end
     volunteer_hashes_arr
   end
 
   def save
+    pg_result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+    @id = pg_result[0]["id"].to_i()
   end
 
   def update
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -6,14 +6,14 @@ describe('Volunteer') do
 
   describe("#name") do
     it("will return the name of the volunteer") do
-      test_volunteer = Volunteer.new({'name' => 'Taylor Swift', 'id' => nil})
+      test_volunteer = Volunteer.new({:name => 'Taylor Swift', :id => nil})
       expect(test_volunteer.name).to(eq("Taylor Swift"))
     end
   end
 
   describe("#id") do
     it("will return the id of a volunteer") do
-      test_volunteer = Volunteer.new({'name' => 'Taylor Swift', 'id' => nil})
+      test_volunteer = Volunteer.new({:name => 'Taylor Swift', :id => nil})
       expect(test_volunteer.id).to(eq(nil))
     end
   end
@@ -24,7 +24,12 @@ describe('Volunteer') do
     end
   end
 
+  describe('#save') do
+    it("will save the volunteer to the database") do
+      test_volunteer = Volunteer.new({:name => 'Taylor Swift', :id => nil})
+      test_volunteer.save
+      expect(Volunteer.all[0].id).to(eq(test_volunteer.id))
+    end
+  end
+
 end
-
-
-# test_volunteer = Volunteer.new({'name' => 'Taylor Swift', 'id' => nil})
